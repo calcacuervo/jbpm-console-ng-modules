7fc1e11ec8f726f64d284b28cc22057d20b4d682
nicolasegomez <nicolas.e.gomez@gmail.com>
2014-03-30 23:04:45 -0300

    Sigue sin andar

--- jbpm-console-ng-documents/jbpm-console-ng-documents-backend/src/main/java/org/jbpm/console/ng/documents/backend/server/DocumentServiceEntryPointImpl.java
+++ jbpm-console-ng-documents/jbpm-console-ng-documents-backend/src/main/java/org/jbpm/console/ng/documents/backend/server/DocumentServiceEntryPointImpl.java
@@ -1,19 +1,9 @@
 package org.jbpm.console.ng.documents.backend.server;
 
-import org.jbpm.console.ng.documents.service.DocumentServiceEntryPoint;
-
-
-
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.enterprise.context.ApplicationScoped;
+
 import org.jboss.errai.bus.server.annotations.Service;
+import org.jbpm.console.ng.documents.service.DocumentServiceEntryPoint;
 
 @Service
 @ApplicationScoped
